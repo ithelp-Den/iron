1ca17a429ddaa7ca5fdcc275875c08c9dae3fbdf
ithelp-Den <it_help@zbl.agency>
2025-12-17 16:09:39 +0100

    Add files via upload

---
+++ js/anim-steps.js
@@ -0,0 +1,22 @@
+const steps = document.querySelectorAll('.s4-step');
+
+const observer = new IntersectionObserver(
+  (entries, obs) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const index = [...steps].indexOf(entry.target);
+
+        setTimeout(() => {
+          entry.target.classList.add('is-visible');
+        }, index * 250); // послідовність
+
+        obs.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.4 // момент появи
+  }
+);
+
+steps.forEach(step => observer.observe(step));
